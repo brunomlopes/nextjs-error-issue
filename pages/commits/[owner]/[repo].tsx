7318a635063820edf42ab57e3e144d77ce89bbed
Bruno Lopes <brunomlopes@gmail.com>
2023-06-02 17:15:19 +0100

    Trying to get some middleware to work

--- pages/commits/[owner]/[repo].tsx
+++ pages/commits/[owner]/[repo].tsx
@@ -1,45 +1,39 @@
-import { GetServerSideProps } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { getRepositoryCommits } from '../../../utils/githubAPI';
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import {
+  ICommit,
+  RepositoryCommits,
+  getRepositoryCommits,
+} from "../../../utils/githubAPI";
 
-type Commit = {
-  sha: string;
-  commit: {
-    message: string;
-    author: {
-      name: string;
-      date: string;
-    };
-  };
-  html_url: string;
-};
+interface RepoCommitsProps {
+  commits: ICommit[];
+}
 
-const RepoCommits = () => {
+const RepoCommits: NextPage<RepoCommitsProps> = ({ commits }) => {
   const router = useRouter();
-  const { owner, repo } = router.query;
-  
-  const [commits, setCommits] = useState<Commit[]>([]);
+  // const { owner, repo } = router.query;
 
-  useEffect(() => {
-    const fetchCommits = async () => {
-      const commits = await getRepositoryCommits(owner as string, repo as string);
-      setCommits(commits);
-    };
+  // const [commits, setCommits] = useState<Commit[]>([]);
 
-    fetchCommits();
-  }, [owner, repo]);
-
+  // useEffect(() => {}, [owner, repo]);
+  const owner = "test";
+  const repo = "testrepo";
   return (
     <div>
-      <h1>Commits for {owner}/{repo}</h1>
+      <h1>
+        Commits for {owner}/{repo}
+      </h1>
       <ul>
-        {commits.map(commit => (
+        {commits.map((commit) => (
           <li key={commit.sha}>
             <p>Commit: {commit.sha}</p>
             <p>Author: {commit.commit.author.name}</p>
             <p>Message: {commit.commit.message}</p>
-            <a href={commit.html_url} target="_blank">See on GitHub</a>
+            <a href={commit.html_url} target="_blank">
+              See on GitHub
+            </a>
           </li>
         ))}
       </ul>
@@ -47,4 +41,62 @@ const RepoCommits = () => {
   );
 };
 
+const withInnovationCastContext =
+  <T, D = []>({
+    fn,
+    requests = () => [],
+    pageOptions = {},
+  }: {
+    fn: ICContextCallbackFnType<T, D>;
+    requests?: (
+      context: GetServerSidePropsContext,
+      siteConfig: SiteConfig
+    ) => any[];
+    pageOptions?: PageOptions;
+  }) =>
+  async (context: GetServerSidePropsContext) => {
+    const dataPromise = Promise.all([
+      ...requests(context, {}).map(async (request) =>
+        request ? await request() : undefined
+      ),
+    ]);
+
+    const data: unknown[] = await dataPromise;
+
+    return fn({}, data as unknown as D);
+  };
+
+export const getServerSideProps: GetServerSideProps = withInnovationCastContext<
+  {},
+  [{ commits: ICommit[] }]
+>({
+  fn: async (d, data) => {
+    const commits = data[0] || null;
+    return { props: { commits } };
+  },
+  requests: (ctx, config) => {
+    const { owner, repo } = ctx.query;
+
+    const fetchCommits = async () => {
+      const commits = await getRepositoryCommits(
+        owner as string,
+        repo as string
+      );
+      return commits;
+    };
+
+    return [fetchCommits];
+  },
+});
+class SiteConfig {}
+
+class PageOptions {}
+
+interface ICGetServerSidePropsContext {}
+
+type ICContextCallbackFnType<T, D> = {
+  (content: ICGetServerSidePropsContext, loadedData: D): Promise<{
+    props: T;
+  }>;
+};
 export default RepoCommits;
--- utils/githubAPI.ts
+++ utils/githubAPI.ts
@@ -1,10 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 const githubAPI = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: "https://api.github.com",
 });
 
-export const getRepositoryCommits = async (owner: string, repo: string) => {
+export interface ICommit {
+  sha: string;
+  commit: {
+    message: string;
+    author: {
+      name: string;
+      date: string;
+    };
+  };
+  html_url: string;
+}
+
+export type RepositoryCommits = {
+  commits: ICommit[];
+};
+
+export async function getRepositoryCommits(
+  owner: string,
+  repo: string
+): Promise<RepositoryCommits> {
   const response = await githubAPI.get(`/repos/${owner}/${repo}/commits`);
   return response.data;
-};
+}
